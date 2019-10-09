d55b62beb2506dc2aa495e9c8170a3ce5d39e91f
Anna Yoakum <annayoakum@ufl.edu>
2019-10-08 21:14:33 -0500

    Clean up structure and finalize code

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
-console.log( apiResult);
+
+        console.log( apiResult);
 
         //API Code
         var sectionOne = document.getElementById('header');
@@ -20,5 +21,6 @@ console.log( apiResult);
 
     }
 };
+
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=39208,us&appid=6efff70fe1477748e31c17d1c504635f', true);
 xmlhttp.send();
