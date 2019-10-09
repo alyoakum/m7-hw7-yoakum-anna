5493f8345e1c0b90c1438e4a9a4a572ca9192edb
Anna Yoakum <annayoakum@ufl.edu>
2019-10-08 21:13:26 -0500

    Add scripts for API, additional h3 and print city and weather to the DOM

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
+var mainSection = document.getElementById('main-body');
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
+console.log( apiResult);
 
-        //code API data goes here
+        //API Code
+        var sectionOne = document.getElementById('header');
+        var addName = document.createElement('h3');
+        sectionOne.appendChild(addName);
+        var text= document.createTextNode(apiResult.name);
+        addName.appendChild(text);
+
+        var sectionTwo = document.getElementById('main');
+        var addWeather = document.createElement('h3');
+        sectionTwo.appendChild(addWeather);
+        var text1= document.createTextNode(apiResult.weather[0].description);
+        addWeather.appendChild(text1);
 
     }
 };
